aab11ea847e5a9b7c7c5749fe07e6d2268f5b9a3
jaydipmaru <156058844+jaydip-maru@users.noreply.github.com>
2026-02-28 13:44:10 +0530

    Fix API URL in DashBoard component

--- frontend/src/components/DashBoard/DashBoard.jsx
+++ frontend/src/components/DashBoard/DashBoard.jsx
@@ -38,7 +38,7 @@ function DashBoard() {
           navigate("/");
         }
         const { data } = await axios.post(
-          `${import.meta.env.VITE_API_URL}/`,
+          ${import.meta.env.VITE_API_URL},
           {},
           { withCredentials: true }
           );
